a9dfdd69aa59585d697669e20acdae262c136c55
xwlmdd <1072552712@qq.com>
2018-06-17 16:24:11 +0800

    博客数据层代码

--- resource-share-service/src/main/java/com/mdd/share/mapper/BlogMapper.java
+++ resource-share-service/src/main/java/com/mdd/share/mapper/BlogMapper.java
@@ -1,15 +1,23 @@
 package com.mdd.share.mapper;
 
 import com.mdd.share.model.Blog;
+import com.mdd.share.model.QueryConditon;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public interface BlogMapper {
     int deleteByPrimaryKey(Integer id);
 
     int insert(Blog record);
 
-    int insertSelective(Blog record);
-
     Blog selectByPrimaryKey(Integer id);
 
     Blog queryBlogByTitleAndTime(Blog blog);
+
+    List<Blog> getAllBlog(QueryConditon queryConditon);
+
+    List<Blog> selectByIds(ArrayList<Integer> idList);
+
+    List<Blog> recommendBlog(String kind, Integer limit);
 }
--- resource-share-service/src/main/java/com/mdd/share/service/BlogService.java
+++ resource-share-service/src/main/java/com/mdd/share/service/BlogService.java
@@ -1,6 +1,10 @@
 package com.mdd.share.service;
 
 import com.mdd.share.model.Blog;
+import com.mdd.share.model.QueryConditon;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/5/9.
@@ -19,4 +23,27 @@ public interface BlogService {
      * @return
      */
     boolean isExistBlog(Blog blog);
+
+    /**
+     * 分页获取博客
+     * @param queryConditon
+     * @return
+     */
+    List<Blog> getAllBlog(QueryConditon queryConditon);
+
+    /**
+     * 根据id获取博客
+     * @param idList
+     * @return
+     */
+    List<Blog> getPreAndLastBlogById(ArrayList<Integer> idList);
+
+    /**
+     * 获取相关推荐书籍
+     *
+     * @param kind
+     * @param limit
+     * @return
+     */
+    List<Blog> recommendBlog(String kind, Integer limit);
 }
